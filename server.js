1a55ee56999ba5fb1dc885b9511e04391a4b7dd2
siberia <1322801233@qq.com>
2018-04-13 02:29:15 +0800

    server.js

--- server.js
+++ server.js
@@ -32,17 +32,42 @@ var server = http.createServer(function(request, response){
 
 
   console.log('方方说：得到 HTTP 路径\n' + path)
-  console.log('方方说：查询字符串为\n' + query)
-  console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+  // console.log('方方说：查询字符串为\n' + query)
+  // console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
   if (path == '/') {
      response.write('Hi\n')
+     response.end()
+  } else if (path == '/style') {
+     response.setHeader('Content-Type', 'text/css; charset=utf-8')
+     response.write('body{background-color: #000;}h1{color: #fff;}')
+     response.end()
+  } else if (path == '/script') {
+     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+     response.write('alert("JS已执行")')
+     response.end()
+  } else if (path == '/index'){
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write('<!DOCTYPE>\n<html>' +
+     '<head><link rel="stylesheet" href="/style">' +
+     '</head><body>' +
+     '<h1>👉  Hello Node.js 你好  👋</h1>' +
+     '<script src="/script"></script>' +
+     '</body></html>')
+    response.end()
      response.end()
   } else {
      response.statusCode = 404
      response.end()
   }
 
-
+  // response.setHeader('Content-Type', 'text/html; charset=utf-8')
+  // response.write('<!DOCTYPE>\n<html>' +
+  //  '<head><link rel="stylesheet" href="/style">' +
+  //  '</head><body>' +
+  //  '<h1>👉Hello Node.js 你好 👋</h1>' +
+  //  '<script src="/script"></script>' +
+  //  '</body></html>')
+  // response.end()
 
 
 
