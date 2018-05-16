7226f8443fc53cb2bed33e5969c9dc3c482493f3
siberia <1322801233@qq.com>
2018-05-16 22:25:52 +0800

    DOM-优化

--- DOM/DOMtransmit/main.js
+++ DOM/DOMtransmit/main.js
@@ -1,57 +1,21 @@
-$(clickMe).on('click', function() {
-   $(popover).toggleClass("active")
-   if($(popover).hasClass("active")){
-     setTimeout(function() {
-       $(document).one('click', function() {
-         $(popover).removeClass("active")
-       })
-     }, 0)
-   }
-})
+showPopover("#clickMe", "#popover", "#wraper")
 
-$(wrapper).on('click', function(e){
-  e.stopPropagation()
-})
+function showPopover(btn, popover, wraper){
+  $(btn).on('click', function() {
+     $(popover).toggleClass("active")
+     if($(popover).hasClass("active")){
+       setTimeout(function() {
+         $(document).one('click', function() {
+           $(popover).removeClass("active")
+         })
+       }, 0)
+     }
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//简单的方法
-// clickMe.addEventListener('click', function(e) {
-//   popover.style.display = 'block'
-//   e.stopPropagation()
-// })
-// wrapper.addEventListener('click', function(e) {
-//   e.stopPropagation()
-// })
-// document.addEventListener('click', function() {
-//   popover.style.display = 'none'
-// })
-
-
-
-
+  $(wrapper).on('click', function(e){
+    e.stopPropagation()
+  })
+}
 
 
 
