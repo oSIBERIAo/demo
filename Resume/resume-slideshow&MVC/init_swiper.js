ad6bc1049a7f2822fa26b9be3cc1ff0ea0ac313b
siberia <1322801233@qq.com>
2018-05-25 00:12:27 +0800

    第二版 加轮播图

---
+++ Resume/resume-slideshow&MVC/init_swiper.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper ('.swiper-container', {
+  // direction: 'vertical',
+  loop: true,
+
+  // 如果需要分页器
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
