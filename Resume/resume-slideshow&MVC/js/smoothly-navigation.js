3e3b33dc64ecb2334bf8a28e54778b16eec9a3ca
siberia <siberia@siberiadeMacBook-Air.local>
2018-05-28 17:25:43 +0800

    模块化-完成✅

--- Resume/resume-slideshow&MVC/js/smoothly-navigation.js
+++ Resume/resume-slideshow&MVC/js/smoothly-navigation.js
@@ -1,11 +1,21 @@
 !function (){
-  let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-  for (let i = 0; i < aTags.length; i++) {
-    aTags[i].onclick = function(x) {
-      x.preventDefault()
-      let a = x.currentTarget
-      let href = a.getAttribute('href')
-      let element = document.querySelector(href)
+  var view = document.querySelector('nav.menu')
+  var controller = {
+    view: null,
+    aTags: null,
+    init: function(view) {
+      this.view = view
+      this.initAnimation()
+      this.bandEvents()
+    },
+    initAnimation: function() {
+      function animate(time) {
+          requestAnimationFrame(animate);
+          TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+    },
+    scrollToElement: function(element) {
       let top = element.offsetTop
 
       let currentTop = window.scrollY
@@ -15,11 +25,7 @@
       let t = Math.abs(s/100)*500  //时间
       if(t > 1000) { t = 1000 }
 
-      function animate(time) {
-          requestAnimationFrame(animate);
-          TWEEN.update(time);
-      }
-      requestAnimationFrame(animate);
+
       var coords = { y: currentTop };  //启始位置
       var tween = new TWEEN.Tween(coords)
         .to({y: targetTop }, t)  //结束位置， 时间
@@ -30,6 +36,20 @@
             window.scrollTo(0, coords.y)  //更新坐标
         })
         .start();  //开始缓动
-    }
+    },
+    bandEvents: function() {
+      let aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+      for (let i = 0; i < aTags.length; i++) {
+        aTags[i].onclick = (x)=>{
+          x.preventDefault()
+          let a = x.currentTarget
+          let href = a.getAttribute('href')
+          let element = document.querySelector(href)
+          this.scrollToElement(element)
+        }
+      }
+    },
   }
+
+  controller.init.call(controller, view)
 }.call()
