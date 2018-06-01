6a2ecaea138281efc4be3f8a3da0411523f5df17
siberia <siberia@siberiadeMacBook-Air.local>
2018-06-01 17:56:46 +0800

    一半

--- nodejs-server-test/nodejs-test-sign-in-demo/server.js
+++ nodejs-server-test/nodejs-test-sign-in-demo/server.js
@@ -52,12 +52,69 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'application/jacascript;charset=utf-8')
     response.write(string)
     response.end()
-  } else if (path === '/sign_up') {
+  } else if (path === '/sign_up' && method === 'GET') {
     var string = fs.readFileSync('./sign_up.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
+  } else if (path === '/sign_up' && method === 'POST') {
+    readBody(request).then( (body)=>{
+      let strings = body.split('&')
+      let hash = []
+      strings.forEach( (string)=>{
+        let parts = string.split('=')
+        let key =  parts[0]
+        let value = parts[1]
+        hash[key] = decodeURIComponent(value) //转码
+      })
+      console.log(hash);
+      // let email = hash['email']
+      // let password = hash['password']
+      // let password_confirmation = hash['password_confirmation']
+      let {email, password, password_confirmation} = hash
+      if(email.indexOf('@') === -1){
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'application/json;charset=utf-8')
+        response.write(`{
+          "errors": {
+            "email": "invalid"
+          }
+        }`)
+      } else if(password!== password_confirmation) {
+        response.statusCode = 400
+        response.write('password not match')
+      } else {
+        var users = fs.readFileSync('./db/users', 'utf8')
+        console.log('users', users);
+        try {
+          users = JSON.parse(users)
+        } catch (exception) {
+          users = []
+        }
+        let inUse = false
+        for (let i = 0; i < users.length; i++) {
+          let user[i] = users[i]
+          if (user.email === email) {
+            inUse = ture
+            break
+          }
+        }
+        if (inUse) {
+          response.statusCode = 404
+          response.setHeader('Content-Type', 'application/jacascript;charset=utf-8')
+          response.write('呜呜呜')
+          response.end()
+        }
+        users.push({email: email, password: password})
+        var usersString = JSON.stringify(users)
+        console.log('usersString', usersString);
+        fs.writeFileSync('./db/users', usersString)
+
+        response.statusCode = 200
+      }
+      response.end()
+    })
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'application/jacascript;charset=utf-8')
@@ -66,6 +123,18 @@ var server = http.createServer(function(request, response){
   }
   /******** 代码结束，下面不要看 ************/
 })
+
+function readBody(request) {
+  let body = []
+  return new Promise((resolve, reject)=>{
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      resolve(body)
+    });
+  })
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
