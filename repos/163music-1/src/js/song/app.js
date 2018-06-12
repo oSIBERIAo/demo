d3a7e0e705ad183f82732fc68d4e1af1de13921e
siberia <1322801233@qq.com>
2018-06-13 03:31:52 +0800

    播放与暂停

--- repos/163music-1/src/js/song/app.js
+++ repos/163music-1/src/js/song/app.js
@@ -1,5 +1,25 @@
 {
-  let view = {}
+  let view = {
+    el: '#app',
+    template: `
+     <audio controls src={{url}}></audio>
+     <div>
+       <button class="play">播放</button>
+       <button class="pause">暂停</button>
+     </div>
+    `,
+    render(data){
+      $(this.el).html(this.template.replace('{{url}}', data.url))
+    },
+    play(){
+      let audio = $(this.el).find('audio')[0]
+      audio.play()
+    },
+    pause(){
+      let audio = $(this.el).find('audio')[0]
+      audio.pause()
+    },
+  }
   let model = {
     data: {
       id: '',
@@ -14,8 +34,10 @@
     get(){
       var query = new AV.Query('Song');
       console.log('this.data.id', this.data.id);
-       query.get(this.data.id).then(function (song) {
-         console.log(song);
+       return query.get(this.data.id).then((song)=>{
+         console.log('song', song);
+         Object.assign(this.data, {id: song.id, ...song.attributes})
+         return song
        }, function (error) {
          // 异常处理
        });
@@ -28,14 +50,29 @@
       let id = this.getSongId()
 
       this.model.setId(id)
-      this.model.get()
+      this.model.get().then(()=>{
+        console.log('this.model.data', this.model.data);
+        this.view.render(this.model.data)
+        // setTimeout(()=>{
+        //   this.view.play()
+        // },3000)
+      })
+      this.bindEvents()
+    },
+    bindEvents(){
+      $(this.view.el).on('click', '.play', ()=>{
+        this.view.play()
+      })
+      $(this.view.el).on('click', '.pause', ()=>{
+        this.view.pause()
+      })
     },
     getSongId(){
       let search = window.location.search
       if (search.indexOf('?') === 0) {
         search = search.substring(1)
       }
-      let array = search.split('&').filter((v=>v))
+      let array = search.split('&').filter((v=>v)) // .filter((v)=>{return !!v})
       let id = ''
 
       for (var i = 0; i < array.length; i++) {
