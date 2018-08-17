5c6ae806581c1f521aa51acef2c6ab2a442e47c1
siberia <1322801233@qq.com>
2018-08-18 00:21:42 +0800

    添加滚动对应导航栏高亮、滚动动画

--- Resume-V1/resume-1/js/main.js
+++ Resume-V1/resume-1/js/main.js
@@ -1,17 +1,39 @@
+let specialTags = document.querySelectorAll('[data-x]')
+for (var i = 0; i < specialTags.length; i++) {
+  specialTags[i].classList.add("offset")
+}
+setTimeout(function () {
+  findClosest()
+}, 1000)
 window.onscroll = function (e) {
-    // console.log(e)
-    // console.log(window.scrollY) 
     if (window.scrollY > 0) {
-        topNavBar.classList.add("topNavBarActive") 
+        topNavBar.classList.add("topNavBarActive")
     } else {
-        topNavBar.classList.remove("topNavBarActive") 
+        topNavBar.classList.remove("topNavBarActive")
     }
-    // console.log('window.scrollY', window.scrollY);
-    // var aa = document.querySelectorAll('a[href="#portfolio"]')
-    // console.log(aa);
-    
+    findClosest()
 }
 
+function findClosest() {
+  //绝对值最近的 = minIndex
+  let minIndex = 0
+  for (var i = 0; i < specialTags.length; i++) {
+    if (Math.abs(specialTags[i].offsetTop - window.scrollY + 55) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY + 55)) {
+      minIndex = i
+    }
+  }
+  specialTags[minIndex].classList.remove('offset')
+  let id = specialTags[minIndex].id
+  let a = document.querySelector('a[href="#'+ id +'"]')
+
+  if(a){
+    let ul = a.parentNode.parentNode.children
+    for (var i = 0; i < ul.length; i++) {
+      ul[i].classList.remove("active")
+    }
+    a.parentNode.classList.add("active")
+  }
+}
 function animate(time) {
     requestAnimationFrame(animate);
     TWEEN.update(time);
@@ -29,56 +51,28 @@ for (let index = 0; index < aTags.length; index++) {
         let element = document.querySelector(ahref)
         let h = element.getBoundingClientRect().y
         let currentTop = window.scrollY
-        // console.log('currentTop', currentTop) 
+
         let top = element.offsetTop
-        
+
         let targetTop = top - 70
         let s = Math.abs(targetTop - currentTop)
-        // let n = 60
-        // let distance = Math.abs(s / n)
-        // let duration = 5
         let time = s / 100 * 600
         if (time > 1000) {
             time = 1000
         }
-        // console.log('currentTop', currentTop);
-        // console.log('top', top);
-        // console.log('s', s);
-        // console.log('distance', distance);
-        
-        // let i = 0
-        // let id = setInterval(()=> {
-        //     if(i === n){
-        //       window.clearInterval(id)
-        //       return  
-        //     } else if (distance === 0){
-        //       window.clearInterval(id)
-        //       return
-        //     } else if (s < 0) {
-        //       window.scroll(0, targetTop) 
-        //       console.log('hahhah');
-        //       window.clearInterval(id)
-        //       return
-        //     }
-        //   i = i + 1
-        //     window.scroll(0, currentTop + distance*i)     
-        // }, duration)
         var coords = { x: 0, y: currentTop }; // Start at (0, 0)
         var tween = new TWEEN.Tween(coords)
             .to({ y: targetTop }, time)
             .easing(TWEEN.Easing.Exponential.Out)
-            .onUpdate(function () {  
+            .onUpdate(function () {
                 addEventListener("scroll", function (params) {
-                })              
+                })
                 window.scroll(0, coords.y)
             })
             .start();
-
-        // window.scroll(0, top - 70)
-        // console.log('top', top); 
-        // console.log('h', h);    
-        // console.log('a', a);
-        // console.log('ahref', ahref);
-        // console.log('e', e); 
-    }   
+    }
 }
+
+
+
+//
