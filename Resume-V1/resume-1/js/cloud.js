08842ffda50487307f7b89cd35c7f9acc3cc7d8c
siberia <1322801233@qq.com>
2018-08-22 20:57:03 +0800

    添加留言，未完

--- Resume-V1/resume-1/js/cloud.js
+++ Resume-V1/resume-1/js/cloud.js
@@ -30,36 +30,51 @@ testObject.save({
 //   console.log('LeanCloud Rocks!');
 // })
 
+
+
+// 第一个参数是 className，第二个参数是 objectId
+var todo = AV.Object.createWithoutData('Message');
+
+function xxx() {
+  todo.fetch().then(function (e) {
+    let response = JSON.parse(e._hashedJSON.results)
+    console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      let {user, message} = response[i]
+      console.log("user", user);
+      console.log("message", message);
+      let li = document.createElement('li')
+      li.innerHTML = message
+      console.log('li',li);
+      messageList.children[0].append(li)
+    }
+  }, function (error) {
+    // 异常处理
+  });
+}
+xxx()
+
 messageSubmit.addEventListener('submit',function (e) {
   e.preventDefault()
   var messageValue = document.getElementById("messageValue").value
   var User = document.getElementById("User").value
+  if (messageValue === "") {
+    alert('留言不能为空哦')
+    return
+  }
   let Message = AV.Object.extend('Message');
   let message= new Message();
   message.save({
     message: messageValue,
     user: User,
   }).then(function(object) {
-    alert('message Rocks!');
+    let {user, message} = object.attributes
+    let li = document.createElement('li')
+    li.innerHTML = message
+    messageList.children[0].append(li)
+    var scrollHeight = $('#xxxx').prop("scrollHeight");
+    $('#xxxx').scrollTop(scrollHeight,0);
   })
 })
+
 //
-
-
-// 第一个参数是 className，第二个参数是 objectId
-var todo = AV.Object.createWithoutData('Message');
-todo.fetch().then(function (e) {
-  let response = JSON.parse(e._hashedJSON.results)
-  console.log(response);
-  for (var i = 0; i < response.length; i++) {
-    let {user, message} = response[i]
-    console.log("user", user);
-    console.log("message", message);
-    let li = document.createElement('li')
-    li.innerHTML = user
-    console.log('li',li);
-    messageList.children[0].append(li)
-  }
-}, function (error) {
-  // 异常处理
-});
